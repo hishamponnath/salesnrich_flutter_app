dcd311c9b3d1c484aa15071aedcf0e6478fa662c
hisham <hishamaitrich@gmail.com>
2024-07-11 16:03:23 +0530

    home ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:salesnrich_app_flutter/view/home_view.dart';
 import 'package:salesnrich_app_flutter/view/login_view.dart';
 import 'package:salesnrich_app_flutter/view/splash_view.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SplashScreen(),
+    home: homescreen(),
   ));
 }
---
+++ lib/view/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class homescreen extends StatefulWidget {
+  const homescreen({super.key});
+
+  @override
+  State<homescreen> createState() => _homescreenState();
+}
+
+class _homescreenState extends State<homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 135, 167, 193),
+        title: const Text("Hisham"),
+      ),
+    );
+  }
+}
