b13a8876a07f549ee9429e3e5159bc8309ce4206
aswinshibish <aswinpsaitrich@gmail.com>
2024-07-27 04:09:34 -0700

    attendancescreen and punchoutscreen modified without API

--- lib/view/attendancescreen_view.dart
+++ lib/view/attendancescreen_view.dart
@@ -12,87 +12,133 @@ class AttendanceScreen extends StatelessWidget {
         title: const Text("Attendance"),
       ),
       drawer: const Drawer(child: Drawerclass()),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Attendance",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              Card(
+                elevation: 4, // Shadow effect
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      DropdownButtonFormField<String>(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Attendance Status",
+                        ),
+                        items: const [
+                          DropdownMenuItem(
+                              value: "PRESENT", child: Text("PRESENT")),
+                          DropdownMenuItem(
+                              value: "LEAVE/OFF", child: Text("LEAVE/OFF")),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                      const SizedBox(
+                          height: 180), // Space between Dropdown and TextField
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Your Remarks",
+                        ),
+                        maxLines: 4,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text(
-              "Attendance",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Attendance Status",
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text("View"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                ),
               ),
-              items: const [
-                DropdownMenuItem(value: "PRESENT", child: Text("PRESENT")),
-                DropdownMenuItem(value: "LEAVE/OFF", child: Text("LEAVE/OFF")),
-              ],
-              onChanged: (value) {},
             ),
-            const SizedBox(height: 400),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Your Remarks",
+            SizedBox(width: 8), // Reduced space between buttons
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text("Send"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.greenAccent,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                ),
               ),
-              maxLines: 4,
             ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 40,
-                        vertical: 15,
-                      ),
-                    ),
-                    child: const Text("VIEW"),
-                  ),
+            SizedBox(width: 8), // Reduced space between buttons
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.photo,
+                  color: Colors.white,
+                  size: 20, // Adjusted icon size
                 ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 40,
-                        vertical: 15,
-                      ),
-                    ),
-                    child: const Text("SEND"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
+                  padding: EdgeInsets.all(
+                      8.0), // Added padding to ensure icon is centered
                 ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                    padding: const EdgeInsets.all(15),
-                    shape: const CircleBorder(),
+              ),
+            ),
+            SizedBox(width: 8), // Reduced space between buttons
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.camera_alt,
+                  color: Colors.white,
+                  size: 20, // Adjusted icon size
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purpleAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                  child: const Icon(Icons.photo),
+                  padding: EdgeInsets.all(
+                      8.0), // Added padding to ensure icon is centered
                 ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                    padding: const EdgeInsets.all(15),
-                    shape: const CircleBorder(),
-                  ),
-                  child: const Icon(Icons.camera_alt),
-                ),
-              ],
+              ),
             ),
           ],
         ),
--- lib/view/punchoutscreen_view.dart
+++ lib/view/punchoutscreen_view.dart
@@ -16,71 +16,109 @@ class PunchOutScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            const Text(
-              "Punch Out",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            Card(
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      "Punch Out",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 160),
+                    const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Your Remarks",
+                      ),
+                      maxLines: 4,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 450),
-            const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Your Remarks",
-              ),
-              maxLines: 4,
             ),
             const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                      ),
-                    ),
-                    child: const Text("VIEW"),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 8.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text("View"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
                 ),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                      ),
-                    ),
-                    child: const Text("PUNCH OUT"),
+              ),
+            ),
+            SizedBox(width: 8),
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text("PunchOut"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.greenAccent,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
                 ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                    padding: const EdgeInsets.all(15),
-                    shape: const CircleBorder(),
+              ),
+            ),
+            SizedBox(width: 8),
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.photo,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                  child: const Icon(Icons.photo),
+                  padding: EdgeInsets.all(8.0),
                 ),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                    padding: const EdgeInsets.all(15),
-                    shape: const CircleBorder(),
+              ),
+            ),
+            SizedBox(width: 8),
+            Flexible(
+              flex: 1,
+              child: TextButton(
+                onPressed: () {},
+                child: Icon(
+                  Icons.camera_alt,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purpleAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                  child: const Icon(Icons.camera_alt),
+                  padding: EdgeInsets.all(8.0),
                 ),
-              ],
+              ),
             ),
           ],
         ),
